456fbddb8ed7eaeedeec584226968570241ae4b0
Keiner0115 <keinermanotas09@gmail.com>
2024-09-10 16:45:46 -0500

    ruta de creada por eday

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AboutPage } from './component/AboutPage.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+
+  {
+    path: "/about",
+    element: <AboutPage/>,
+  },
+
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    < RouterProvider router={router} />
   </StrictMode>,
 )
